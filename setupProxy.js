d7dd59767021e49d3c71c77f3190fb13d54af6fb
Yash Sahane <yashsahane23@gmail.com>
2023-05-05 14:30:32 +0530

    fixing

---
+++ setupProxy.js
@@ -0,0 +1,11 @@
+import createProxyMiddleware from './proxyMiddleware';
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
